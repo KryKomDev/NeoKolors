9d100f6970afcabbdca2603b415147b5cf5c73d5
KryKomDev <krystof.komzak@gmail.com>
2025-01-19 10:23:39 +0100

    added icons

---
+++ NeoKolors.Common/Exceptions/InvalidColorCastException.cs
@@ -0,0 +1,8 @@
+﻿namespace NeoKolors.Common.Exceptions;
+
+public class InvalidColorCastException : InvalidCastException {
+    private InvalidColorCastException(string message) : base(message) { }
+    
+    public static InvalidColorCastException CustomToConsoleColor() => new("Cannot cast a custom color to ConsoleColor.");
+    public static InvalidColorCastException ConsoleColorToCustom() => new("Cannot cast a ConsoleColor to a custom color.");
+}
